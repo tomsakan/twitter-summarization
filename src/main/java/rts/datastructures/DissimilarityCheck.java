5ff60c1c4734d9709bb378cbbe4bc132c26ec260
tom <tomsakan@gmail.com>
2019-07-31 17:08:16 +0100

    Added Novelty finding

--- src/main/java/rts/datastructures/DissimilarityCheck.java
+++ src/main/java/rts/datastructures/DissimilarityCheck.java
@@ -1,6 +1,13 @@
 package rts.datastructures;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import rts.calculator.CosineSimilarityCalculator;
+import rts.calculator.TFIDFCalculator;
 
 public class DissimilarityCheck implements Serializable{
 
@@ -8,5 +15,59 @@ public class DissimilarityCheck implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 720145350964418763L;
+	
+	public HashMap<String, List<String>> summaries = new HashMap<String, List<String>>();
+	
+	public void addSummary(String topicID, String tweet){
+		if(summaries.get(topicID) == null){
+			summaries.put(topicID, new ArrayList<String>());
+		}
+		
+		summaries.get(topicID).add(tweet);
+	}
+	
+	public boolean checkDissim(String topicID, String tweet){
+		List<String> getSummaries = summaries.get(topicID);
+		TFIDFCalculator tfidf = new TFIDFCalculator();
+		for(String summary : getSummaries){
+			List<String> temp =  Arrays.asList((summary+" "+tweet).split(" "));
+            List<String> terms = new ArrayList<String>();
+            for(String term : temp){
+                if(!(terms.contains(term) || term.equals(" ") || term.equals(""))) terms.add(term);
+            }
+            
+            DFCounter counter = new DFCounter();
+            
+            counter.addTerms(topicID, summary, tweet);
+            
+            double[] sumArr = new double[terms.size()];
+            double[] tweetArr = new double[terms.size()];
+            int i = 0;
+            
+            for(String term : terms){
+              double tfidfSum = tfidf.tfIdf(Arrays.asList(summary.split(" ")), 2, counter.getDF(topicID, term), term);
+              double tfidfTweet = tfidf.tfIdf(Arrays.asList(tweet.split(" ")), 2, counter.getDF(topicID, term), term);
+              sumArr[i] = tfidfSum;
+              tweetArr[i] = tfidfTweet;
+              i++;
+            }
+            
+            double cosine = CosineSimilarityCalculator.cosineSimilarity(sumArr, tweetArr);
+            if(cosine >= 0.8) {
+//            	System.out.println(summary + "\n" + tweet + "\n" + cosine +"\n-----------");
+            	return false;
+            }
+//            System.out.println(summary + "\n" + tweet + "\n" + cosine +"\n-----------");
+		}
+		return true;
+	}
+
+	public HashMap<String, List<String>> getSummaries() {
+		return summaries;
+	}
+
+	public void setSummaries(HashMap<String, List<String>> summaries) {
+		this.summaries = summaries;
+	}
 
 }
--- src/main/java/rts/tasks/SelectSummary.java
+++ src/main/java/rts/tasks/SelectSummary.java
@@ -13,6 +13,7 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.util.Collector;
 
+import rts.datastructures.DissimilarityCheck;
 import rts.datastructures.SummariesList;
 
 public class SelectSummary implements FlatMapFunction<Tuple2<String, JsonNode>, Tuple2<String, String>>{
@@ -23,6 +24,7 @@ public class SelectSummary implements FlatMapFunction<Tuple2<String, JsonNode>, 
 	private static final long serialVersionUID = 1587117239129149532L;
 	
 	SummariesList summaries = new SummariesList();
+	DissimilarityCheck disSim = new DissimilarityCheck();
 	
 	Integer summarySize = 5;
 
@@ -31,35 +33,30 @@ public class SelectSummary implements FlatMapFunction<Tuple2<String, JsonNode>, 
 		
 		if(summaries.getSummaries(node.f0) == null){
 			summaries.addCosineScore(node.f0, node.f1.get("cosine_score").asDouble());
+			disSim.addSummary(node.f0, node.f1.get("text").asText());
 //			System.out.println(node.f0+"\t"+summaries.getCount(node.f0));
 			if(node.f0.equals("RTS48")){
-				System.out.println(node.f1.get("actual_label").asText()+"\t"+node.f1.get("original_text").asText());
+				System.out.println(node.f1.get("cosine_score").asDouble()+"\t"+node.f1.get("actual_label").asText()+"\t"+node.f1.get("original_text").asText());
 				out.collect(new Tuple2<String, String>(node.f1.get("original_text").asText(), node.f1.get("actual_label").asText()));
 			}
 		}else{
 			Double threshold = summaries.getAvergeScore(node.f0);
 //			Double threshold = 0.4;
 			if((node.f1.get("cosine_score").asDouble() >= threshold) && node.f1.get("cosine_score").asDouble() != 0.0){
-//				if(node.f0.equals("RTS48")){
-//					summaries.addCosineScore(node.f0, node.f1.get("cosine_score").asDouble());
-//					System.out.println(summaries.getAvergeScore(node.f0));
-//				}
-//				System.out.println(node);
 				summaries.addCosineScore(node.f0, node.f1.get("cosine_score").asDouble());
-//				System.out.println(node.f1.get("actual_label").asText()+"\t"+node.f1.get("cosine_score").asText());
+//				disSim.addSummary(node.f0, node.f1.get("text").asText());
+
 				if(node.f0.equals("RTS48")){
-					System.out.println(node.f1.get("actual_label").asText()+"\t"+node.f1.get("original_text").asText());
-					out.collect(new Tuple2<String, String>(node.f1.get("original_text").asText(), node.f1.get("actual_label").asText()));
+//					System.out.println(disSim.checkDissim(node.f0, node.f1.get("text").asText()));
+					if(disSim.checkDissim(node.f0, node.f1.get("text").asText())){
+						disSim.addSummary(node.f0, node.f1.get("text").asText());
+						out.collect(new Tuple2<String, String>(node.f1.get("original_text").asText(), node.f1.get("actual_label").asText()));
+						System.out.println(node.f1.get("cosine_score").asDouble()+"\t"+node.f1.get("actual_label").asText()+"\t"+node.f1.get("original_text").asText());
+					}
 				}
 			}else{
 				summaries.addCosineScore(node.f0, node.f1.get("cosine_score").asDouble());
-//				System.out.println(node.f1.get("actual_label").asText()+"\t"+node.f1.get("cosine_score").asText());
-//				System.out.println("Bye");
 			}
 		}
-		
-		if(node.f0.equals("RTS47")) out.collect(new Tuple2<String, String>(node.f1.get("original_text").asText(), node.f1.get("actual_label").asText()));
-		
-		
 	}
 }
