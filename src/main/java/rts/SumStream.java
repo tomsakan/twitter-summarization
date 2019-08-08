95e6dbf6c7bbc6c5b1284dbfae8229b39d6c98ab
tom <tomsakan@gmail.com>
2019-08-08 22:26:12 +0100

    Add timestamp and watermarks

--- src/main/java/rts/SumStream.java
+++ src/main/java/rts/SumStream.java
@@ -6,11 +6,13 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 
 import rts.tasks.CalculateTFIDF;
 import rts.tasks.CheckLength;
 import rts.tasks.CombinedDescription;
 import rts.tasks.ContainsKeyWords;
+import rts.tasks.ExtractTimeStamp;
 import rts.tasks.FilterNoLabels;
 import rts.tasks.IsEnglish;
 import rts.tasks.PreProcessing;
@@ -29,6 +31,7 @@ public class SumStream {
 		ParameterTool params = ParameterTool.fromArgs(args);
 	    env.getConfig().setGlobalJobParameters(params);
         
+	    
 //	   	get twitter data from a json file
 		DataStreamSource<String> twitterData = env.readTextFile(params.get("inputJson"));
 		
@@ -39,16 +42,20 @@ public class SumStream {
 		
 //		preprocess the tweets which remove urls, @, hashtags, character repetition, words starting with a number etc. Additionally, create
 //		the documents list which contain keys(topics) and the tweets. 
-		DataStream<Tuple2<String, JsonNode>> preprocessed = finalData.map(new PreProcessing(params.get("stopWord")))
-				.filter(new ContainsKeyWords())
-				.filter(new CheckLength())
-				.filter(new IsEnglish());
-//				create a map of strings which share the same topic id key	
-		
-//		DataStream<Tuple2<String, String>> output = preprocessed.keyBy(0).map(new CalculateTFIDF()).flatMap(new SelectSummary());
-		DataStream<String> output = preprocessed.keyBy(0).map(new CalculateTFIDF()).flatMap(new Word2VecTest(params.get("stopWord")));
+//		DataStream<Tuple2<String, JsonNode>> preprocessed = finalData.map(new PreProcessing(params.get("stopWord")))
+//				.filter(new ContainsKeyWords())
+//				.filter(new CheckLength())
+//				.filter(new IsEnglish());
+////				create a map of strings which share the same topic id key	
+//		
+////		DataStream<Tuple2<String, String>> output = preprocessed.keyBy(0).map(new CalculateTFIDF()).flatMap(new SelectSummary());
+//		DataStream<String> output = preprocessed.keyBy(0).map(new CalculateTFIDF()).flatMap(new Word2VecTest(params.get("stopWord")));
 
-		output.writeAsText(params.get("output")).setParallelism(1);
+//		output.writeAsText(params.get("output")).setParallelism(1);
+		
+		DataStream<Tuple2<String, JsonNode>> preprocessed = finalData
+				.assignTimestampsAndWatermarks(new ExtractTimeStamp());
+		
 	    env.execute("Twitter Summarization");
 	}
 }
---
+++ src/main/java/rts/tasks/ExtractTimeStamp.java
@@ -0,0 +1,41 @@
+package rts.tasks;
+
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
+
+public class ExtractTimeStamp implements AssignerWithPeriodicWatermarks<Tuple2<String, JsonNode>>{
+
+	private long currentMaxTimestamp=0;
+	
+	@Override
+	public long extractTimestamp(Tuple2<String, JsonNode> element, long previousElementTimestamp) {
+
+		String time = element.f1.get("created_at").asText();
+		SimpleDateFormat sdf = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy");
+		String dateInString = time;
+		Date date;
+		try {
+			date = sdf.parse(dateInString);
+			return date.getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	public Watermark getCurrentWatermark() {
+		// TODO Auto-generated method stub
+		return new Watermark(currentMaxTimestamp);
+	}
+
+}
